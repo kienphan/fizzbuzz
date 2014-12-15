d3fca3f98462629f66827b6250b53b1c66a4b1dc
kienphan <senior1206@gmail.com>
2014-12-15 23:21:12 +0700

    Fix fizzbuzz controller and fizzbuzz unit test

--- app/controllers/fizzbuzz_controller.rb
+++ app/controllers/fizzbuzz_controller.rb
@@ -2,8 +2,8 @@ class FizzbuzzController < ApplicationController
 
   def index
     @_fizzbuzz_1 = Array.new
-  	1.upto(1000) do |i|
-      @_fizzbuzz_1 << 
+    1.upto(1000) do |i|
+      @_fizzbuzz_1 <<
         if i % 3 == 0
           i % 5 == 0 ? "fizzbuzz" : "fizz"
         else
@@ -15,17 +15,13 @@ class FizzbuzzController < ApplicationController
 
   def extends
     @_fizzbuzz_2 = Array.new
-  	1.upto(1000) do |i|
-      if i.to_s.include? "3"
-        @_fizzbuzz_2 << "fizz"
-      else 
-        @_fizzbuzz_2 << 
-          if i % 3 == 0
-            i % 5 == 0 ? "fizzbuzz" : "fizz"
-          else
-            i % 5 == 0 ? "buzz" : "#{i}"
-          end
-      end
+    1.upto(1000) do |i|
+      @_fizzbuzz_2 << 
+        if i % 5 == 0
+          i % 3 == 0 ? "fizzbuzz" : "buzz"
+        else
+          i % 3 == 0 || ( i.to_s.include? "3" ) ? "fizz" : "#{i}"
+        end
     end
     return @_fizzbuzz_2
   end
--- spec/controllers/fizzbuzz_controller_spec.rb
+++ spec/controllers/fizzbuzz_controller_spec.rb
@@ -11,9 +11,29 @@ RSpec.describe FizzbuzzController, :type => :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
-    it "check fizzbuzz array not nil?" do
+    it "check fizzbuzz array is not nil?" do
       expect(@fizzbuzz).not_to eq nil
     end
+    it "check if element is Fizz" do
+      expect(@fizzbuzz[3 - 1]).to eq 'fizz'
+      expect(@fizzbuzz[24 - 1]).to eq 'fizz'
+      expect(@fizzbuzz[36 - 1]).to eq 'fizz'
+    end
+    it "check if element is Buzz" do
+      expect(@fizzbuzz[5 - 1]).to eq 'buzz'
+      expect(@fizzbuzz[35 - 1]).to eq 'buzz'
+      expect(@fizzbuzz[70 - 1]).to eq 'buzz'
+    end
+    it "check if element is FizzBuzz" do
+      expect(@fizzbuzz[15 - 1]).to eq 'fizzbuzz'
+      expect(@fizzbuzz[60 - 1]).to eq 'fizzbuzz'
+      expect(@fizzbuzz[150 - 1]).to eq 'fizzbuzz'
+    end
+    it "check if element is number" do
+      expect(@fizzbuzz[1 - 1]).to eq '1'
+      expect(@fizzbuzz[7 - 1]).to eq '7'
+      expect(@fizzbuzz[49 - 1]).to eq '49'
+    end 
   end
 
   describe "GET extends" do
@@ -25,9 +45,30 @@ RSpec.describe FizzbuzzController, :type => :controller do
       get :extends
       expect(response).to have_http_status(:success)
     end
-    it "check fizzbuzz array not nil?" do
+    it "check fizzbuzz array is not nil?" do
       expect(@fizzbuzz).not_to eq nil
     end
+    it "check if element is Fizz" do
+      expect(@fizzbuzz[3 - 1]).to eq 'fizz'
+      expect(@fizzbuzz[24 - 1]).to eq 'fizz'
+      expect(@fizzbuzz[37 - 1]).to eq 'fizz'
+      expect(@fizzbuzz[133 - 1]).to eq 'fizz'
+    end
+    it "check if element is Buzz" do
+      expect(@fizzbuzz[5 - 1]).to eq 'buzz'
+      expect(@fizzbuzz[35 - 1]).to eq 'buzz'
+      expect(@fizzbuzz[70 - 1]).to eq 'buzz'
+    end
+    it "check if element is FizzBuzz" do
+      expect(@fizzbuzz[15 - 1]).to eq 'fizzbuzz'
+      expect(@fizzbuzz[60 - 1]).to eq 'fizzbuzz'
+      expect(@fizzbuzz[150 - 1]).to eq 'fizzbuzz'
+    end
+    it "check if element is number" do
+      expect(@fizzbuzz[1 - 1]).to eq '1'
+      expect(@fizzbuzz[7 - 1]).to eq '7'
+      expect(@fizzbuzz[49 - 1]).to eq '49'
+    end 
   end
 
 end
